b09eb96d4eabb9c13d009f0f3ea3a7671379a5d5
elbek <poputchik0103@gmail.com>
2022-04-11 11:40:01 +0300

    ishladimi

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+
+$as ="salom dunyo enga ahahahaha";
+echo  "$as";
+$a = 22;
+$b = 33;
+$d=$a+$b;
+echo "$d";
+$p ="<br > salom olam chi gap";
+echo "$p";
+
+
+
+	 ?>
+	
+</body>
+</html>
